fc0b99b9fa24aef2273a4a5b85e7b13e757c4ab6
Muhammad Asif Shahzad <71362700+aasifshahzad@users.noreply.github.com>
2026-01-21 00:55:53 +0500

    .

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,8 @@ from typing import Any
 
 import emails  # type: ignore
 import jwt
-from jwt import PyJWTError
+from jwt.exceptions import ExpiredSignatureError, DecodeError
+
 from jinja2 import Template
 
 from core import security
